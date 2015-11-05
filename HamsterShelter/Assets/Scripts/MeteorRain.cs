ad53f4380c5e2a6709ae6fbd32d7311bc0733c0e
Joni Roinila <jonroin@utu.fi>
2015-11-05 21:55:06 +0200

    Meteorisade ja palikat lisätty

---
+++ HamsterShelter/Assets/Scripts/MeteorRain.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MeteorRain : MonoBehaviour {
+
+	public GameObject meteori;
+	public Vector3 spawnValue;
+	public int amount;
+	public float wait;
+	public float startWait;
+	
+	void Start () {
+		StartCoroutine (SpawnWaves());
+	}
+	
+	IEnumerator SpawnWaves () {
+		yield return new WaitForSeconds(startWait);
+		for(int i = 0; i < amount; i++)
+		{
+			Vector3 spawnPosition = new Vector3 (Random.Range(-10, 5), spawnValue.y, spawnValue.z);
+			Instantiate(meteori, spawnPosition, Quaternion.identity);
+			yield return new WaitForSeconds(wait);
+		} 
+			
+	}
+	
+	
+}
---
+++ HamsterShelter/Assets/Scripts/WallScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WallScript : MonoBehaviour {
+
+	public int durability;
+	
+	void OnTriggerEnter2D(Collider2D other) {
+		if (durability > 0) {
+			durability = durability - 1;
+		}
+		else {
+		Destroy(gameObject);
+		}
+	}
+}
