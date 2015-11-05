54e4bee219c8e96473a4a0d870e29bfd0f9b4bc2
Joni Roinila <jonroin@utu.fi>
2015-11-05 22:31:44 +0200

    Hamstereille lisätty health scripti

---
+++ HamsterShelter/Assets/Scripts/Health.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Health : MonoBehaviour {
+
+	public int health;
+	
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.tag == "Hazard") {
+			
+			if (health == 0) {
+				Destroy(gameObject);
+			}
+			else {
+				health = health - 1;
+			}
+		}
+		
+	}
+}
