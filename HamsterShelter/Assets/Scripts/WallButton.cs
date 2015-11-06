f5845a1d46dc079734017c2d9bad13d86816c8e1
Matti Liikala <mamilii@utu.fi>
2015-11-06 09:34:53 +0200

    button script added

---
+++ HamsterShelter/Assets/Scripts/WallButton.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WallButton : MonoBehaviour {
+	public GameObject wall;
+	public Vector3 spawnPosition;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void onClick(){
+
+		Instantiate (wall, spawnPosition, Quaternion.identity);
+	}
+}
