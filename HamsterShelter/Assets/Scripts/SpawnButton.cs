6e12167de97fa815068c055d6c0cf48bf03dc1fd
Joni Roinila <joni.roinila@gmail.com>
2015-12-09 16:48:50 +0200

    lisätty hamsterispawni

---
+++ HamsterShelter/Assets/Scripts/SpawnButton.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnButton : MonoBehaviour {
+
+	public GameObject hamsteri;
+	public Vector3 spawnPosition;
+
+
+	public void onClick(){
+
+		Instantiate (hamsteri, spawnPosition, Quaternion.identity);
+	}
+}
