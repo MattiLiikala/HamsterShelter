7aa2a878c10decb64203b4290dcf70a1d0a672b3
Joni Roinila <joni.roinila@gmail.com>
2015-12-10 13:27:57 +0200

    EndLevel scripti lisätty kontrolleriin

---
+++ HamsterShelter/Assets/Scripts/EndLevel.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EndLevel : MonoBehaviour {
+
+
+	public int timeTillNext;
+	public int level;
+	// Use this for initialization
+	void Start () {
+		StartCoroutine (HopToNext());
+	}
+	
+	IEnumerator HopToNext() {
+		yield return new WaitForSeconds(timeTillNext);
+		Application.LoadLevel (level);
+	}
+}
