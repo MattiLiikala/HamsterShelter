3c87493a11c5ef5e4d24fc913055ef96794a4c34
Joni Roinila <joni.roinila@gmail.com>
2015-12-10 11:11:22 +0200

    WallController added

--- HamsterShelter/Assets/Scripts/WallButton.cs
+++ HamsterShelter/Assets/Scripts/WallButton.cs
@@ -4,10 +4,15 @@ using System.Collections;
 public class WallButton : MonoBehaviour {
 	public GameObject wall;
 	public Vector3 spawnPosition;
+	public static int maara;
+	public int max = 5;
 	
 
 	public void onClick(){
-
+		if (maara < max) {
 		Instantiate (wall, spawnPosition, Quaternion.identity);
+		maara = maara + 1;
+		WallController.increaseCount ();
 	}
+}
 }
---
+++ HamsterShelter/Assets/Scripts/WallController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class WallController : MonoBehaviour {
+
+
+	public static int total = 5;
+	public static int count;
+	Text text;
+
+
+	void Awake () {
+		text = GetComponent<Text> ();
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		text.text = "Blocks used: " + count + "/" + total;
+	}
+
+	public static void increaseCount() {
+		count = count + 1;
+
+	}
+		
+
+}
