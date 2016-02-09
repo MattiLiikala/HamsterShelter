009532fe4cf7146bfa27f60bf605e57207a9fe00
unknown <tgl@Media-114.DCWDS.local>
2016-02-09 17:23:04 +0200

    Modified tutorial

--- HamsterShelter/Assets/Scripts/NextStep.cs
+++ HamsterShelter/Assets/Scripts/NextStep.cs
@@ -9,21 +9,25 @@ public class NextStep : MonoBehaviour {
 	public Text teksti;
 	private int counter;
 
+
+
 	public void onClick() {
+
 		counter++;
 		if (counter == 1) {
-			teksti.text = "From the top of the screen you can drag and drop horizontal and vertical building blocks into the ground as well as hamsters. Click to continue";
+			teksti.text = "From the top of the screen you can drag and drop hamsters to the ground. Drag a hamster to the ground and click here to continue";
 		}
 		if (counter == 2) {
-			teksti.text = "On the left bottom corner you see how many blocks you have used and on right bottom you see how many hamsters you have alive. Click to continue";
+			teksti.text = "You can also drag and drop building blocks from top menu. Drag blocks to build a simple shelter for your hamster and click here to continue";
 		}
 		if (counter == 3) {
-			teksti.text = "Next, try dragging and dropping a hamster to the ground and a few blocks to cover it. The armageddon will start in 30 seconds once you close this window. Click to close this window";
+			teksti.text = "If you are happy with your shelter click here and armageddon will start in 5 secods";
 		}
 		if (counter == 4) {
 			Instantiate(sade, paikka, Quaternion.identity);
 			Object.Destroy(this.gameObject);
 		}
 	}
+
 
 }
