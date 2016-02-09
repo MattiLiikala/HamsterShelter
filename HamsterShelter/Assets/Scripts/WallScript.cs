96684f0c500712de19c694cf6ea96970090aa088
Regalis11 <poe.regalis@gmail.com>
2016-02-09 14:34:59 +0200

    Option to change the sprite of a wall when damage is taken

--- HamsterShelter/Assets/Scripts/WallScript.cs
+++ HamsterShelter/Assets/Scripts/WallScript.cs
@@ -1,18 +1,40 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class WallScript : MonoBehaviour, IDamageable {
+public class WallScript : MonoBehaviour, IDamageable
+{
 
-	public int durability;
+	public int Durability;
+
+    private int health;
+
+    public Sprite[] Sprites;
+
+    private SpriteRenderer spriteRenderer;
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+        if (Sprites.Length==0)
+        {
+            Debug.LogError("No sprites configured for wall, using default sprite");
+            Sprites = new Sprite[] { spriteRenderer.sprite };
+        }
+
+        health = Durability;
+    }
 
     public void TakeDamage(int amount)
     {
-        durability -= amount;
+        health -= amount;
 
-        if (durability <= 0)
-        {
-            Destroy(gameObject);
-        }
+        int spriteIndex = Mathf.FloorToInt((1.0f - ((float)health / Durability)) * Sprites.Length);
+
+        spriteRenderer.sprite = Sprites[Mathf.Clamp(spriteIndex, 0, Sprites.Length-1)];
+
+        //todo: add some visual effect when the wall is destroyed (particles, pieces flying off or something)
+        if (health <= 0) Destroy(gameObject);
     }
 	
 }
