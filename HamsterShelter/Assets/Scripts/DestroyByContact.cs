3480379dce4e8ca4252dd4798d8a42ff5ab09db9
Regalis11 <poe.regalis@gmail.com>
2016-02-09 15:25:55 +0200

    Exploding meteors

--- HamsterShelter/Assets/Scripts/DestroyByContact.cs
+++ HamsterShelter/Assets/Scripts/DestroyByContact.cs
@@ -5,13 +5,32 @@ public class DestroyByContact : MonoBehaviour
 {
     public float Delay;
 
+    public GameObject SpawnOnContact;
+
+    private bool destroyed;
+
     void OnCollisionEnter2D(Collision2D other)
     {
-        Destroy(gameObject, Delay);
+        Destroy();
     }
 
-	void OnTriggerEnter2D(Collider2D other) 
+	void OnTriggerEnter2D(Collider2D other)
     {
+        Destroy();
+    }
+
+    private void Destroy()
+    {
+        if (destroyed) return;
+
+        destroyed = true;
+
+        if (SpawnOnContact != null)
+        {
+            Instantiate(SpawnOnContact, transform.position, transform.rotation);
+        }
+
 		Destroy(gameObject, Delay);
 	}
+
 }
