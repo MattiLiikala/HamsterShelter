d6449c0962924d653a4b6f456076ae26bab650e0
Regalis11 <poe.regalis@gmail.com>
2016-02-09 15:08:19 +0200

    Walls break to pieces when destroyed

---
+++ HamsterShelter/Assets/Scripts/ExplodeObject.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExplodeObject : MonoBehaviour
+{
+    public float MinImpulse, MaxImpulse;
+
+	// Use this for initialization
+	void Start ()
+    {
+        var rigidBodies = GetComponentsInChildren<Rigidbody2D>();
+        
+        foreach (Rigidbody2D rigidBody in rigidBodies)
+        {
+            Vector2 impulse = new Vector2(
+                rigidBody.position.x - transform.position.x, 
+                rigidBody.position.y - transform.position.y);
+
+            impulse = impulse.normalized;
+            impulse.x *= Random.Range(MinImpulse, MaxImpulse);
+            impulse.y *= Random.Range(MinImpulse, MaxImpulse);
+
+            rigidBody.AddForce(impulse, ForceMode2D.Impulse);
+        }
+	}
+}
