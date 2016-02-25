d92fb5a6972e1fd25912455c4ee6831287d85c47
Matti Liikala <mamilii@utu.fi>
2016-02-25 12:14:52 +0200

    Screen orientation forced to landscape

--- HamsterShelter/Assets/Scripts/UI/UIManager.cs
+++ HamsterShelter/Assets/Scripts/UI/UIManager.cs
@@ -14,6 +14,7 @@ public class UIManager : MonoBehaviour
     void Awake()
     {
         Instance = this;
+        Screen.orientation = ScreenOrientation.LandscapeLeft;
     }
 
     public void ShowLevelPassedMenu()
