b791190efffd72619669c12c7edc68bd5a0472a3
miolfo <miolfo@utu.fi>
2016-04-09 18:39:07 +0300

    max limit for zoom, simple panning

--- HamsterShelter/Assets/Scripts/UI/GameZoom.cs
+++ HamsterShelter/Assets/Scripts/UI/GameZoom.cs
@@ -5,8 +5,12 @@ public class GameZoom : MonoBehaviour {
 
     //If set to true, this scene can be zoomed by pinching
     public bool ZoomableScene = true;
-    public float ZoomSpeed = 0.5f;
+    public float ZoomSpeed = 0.03f;
+    public float PanSpeed = 0.1f;
     public Camera ZoomCamera;
+
+    private float maxZoom;
+    private bool isZoomed = false;
 
 	// Use this for initialization
 	void Start () {
@@ -16,6 +20,7 @@ public class GameZoom : MonoBehaviour {
             ZoomCamera = GameObject.Find("Main Camera").GetComponent<Camera>();
             if (ZoomCamera == null) throw new System.Exception("Camera for GameZoom not found!");
         }
+        maxZoom = ZoomCamera.orthographicSize;
 	}
 	
 	// Update is called once per frame
@@ -38,6 +43,17 @@ public class GameZoom : MonoBehaviour {
 
                 ZoomCamera.orthographicSize += delta * ZoomSpeed;
                 ZoomCamera.orthographicSize = Mathf.Max(ZoomCamera.orthographicSize, 0.1f);
+                ZoomCamera.orthographicSize = Mathf.Min(ZoomCamera.orthographicSize, maxZoom);
+
+                //Set the isZoomed boolean
+                if (ZoomCamera.orthographicSize == 0.1f || ZoomCamera.orthographicSize == maxZoom) isZoomed = false;
+                else isZoomed = true;
+            }
+            //If the scene is zoomed in, enable moving in scene by touch
+            if(Input.touchCount == 1 && isZoomed)
+            {
+                Vector2 t1 = Input.GetTouch(0).deltaPosition;
+                ZoomCamera.transform.Translate(-t1.x * PanSpeed, -t1.y * PanSpeed, 0);
             }
         }
 	}
